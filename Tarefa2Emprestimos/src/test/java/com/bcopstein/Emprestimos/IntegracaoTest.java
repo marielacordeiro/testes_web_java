980da1d03235081618236671eda87a91ddcb6308
HenriqueTrajano <henriquetrajanoguerra@gmail.com>
2022-10-31 17:20:13 -0300

    start trabson

---
+++ Tarefa2Emprestimos/src/test/java/com/bcopstein/Emprestimos/IntegracaoTest.java
@@ -0,0 +1,90 @@
+package com.bcopstein.Emprestimos;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegracaoTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@ParameterizedTest
+    @CsvSource({
+            "true,false,120,0.02,6,148.8,24.8",
+            "true,false,339000,0.50,2,705120.0,352560.0",
+            "true,false,0.88,0.12,1,1.0472,1.0472",
+            "true,false,5600,0.5,17,54488.0,3205.176470588235",
+            "true,false,0,0.04,1,0,0",
+			"true,false,10,2,2,50.8,25.4",
+    })
+	public void emprestimoJurosSimplesTest(boolean segurado, boolean jurosCompostos, double valor, double taxa, int nroParcelas, double valorTotal, double valorParcela) throws Exception {
+
+		EmprestimoDTO esperado = new EmprestimoDTO(segurado, jurosCompostos, valor, taxa, nroParcelas, valorTotal, valorParcela);
+
+		MvcResult result = this.mockMvc.perform(get("/emprestimo/jurosSimples?valor=" + valor + "&parcelas="+ nroParcelas +"&taxa=" + taxa))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andReturn();
+
+		String content = result.getResponse().getContentAsString();
+		EmprestimoDTO recebido = new Gson().fromJson(content, EmprestimoDTO.class);
+
+		assertEquals(esperado.isSegurado(), recebido.isSegurado());
+		assertEquals(esperado.isJurosCompostos(), recebido.isJurosCompostos());
+		assertEquals(esperado.getValor(), recebido.getValor());
+		assertEquals(esperado.getTaxa(), recebido.getTaxa());
+		assertEquals(esperado.getNroParcelas(), recebido.getNroParcelas());
+		assertEquals(esperado.getValorTotal(), recebido.getValorTotal());
+		assertEquals(esperado.getValorParcela(), recebido.getValorParcela());
+	}
+
+	@ParameterizedTest
+    @CsvSource({
+            "true,true,120,0.02,6,150.48627558348,25.08104593058",
+            "true,true,339000,0.50,2,793293.9,396646.95",
+            "true,true,0.88,0.12,1,1.0472,1.0472",
+            "true,true,5600,0.5,17,6177599.554130504,363388.20906650025",
+            "true,true,0,0.04,1,0,0",
+			"true,true,10,2,2,91.20099999999998,45.60049999999999",
+    })
+	public void emprestimoJurosCompostoTest(boolean segurado, boolean jurosCompostos, double valor, double taxa, int nroParcelas, double valorTotal, double valorParcela) throws Exception {
+
+		EmprestimoDTO esperado = new EmprestimoDTO(segurado, jurosCompostos, valor, taxa, nroParcelas, valorTotal, valorParcela);
+
+		MvcResult result = this.mockMvc.perform(get("/emprestimo/jurosCompostos?valor=" + valor + "&parcelas="+ nroParcelas +"&taxa=" + taxa))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andReturn();
+
+		String content = result.getResponse().getContentAsString();
+		EmprestimoDTO recebido = new Gson().fromJson(content, EmprestimoDTO.class);
+
+		assertEquals(esperado.isSegurado(), recebido.isSegurado());
+		assertEquals(esperado.isJurosCompostos(), recebido.isJurosCompostos());
+		assertEquals(esperado.getValor(), recebido.getValor());
+		assertEquals(esperado.getTaxa(), recebido.getTaxa());
+		assertEquals(esperado.getNroParcelas(), recebido.getNroParcelas());
+		assertEquals(esperado.getValorTotal(), recebido.getValorTotal());
+		assertEquals(esperado.getValorParcela(), recebido.getValorParcela());
+	}
+}
