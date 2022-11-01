bceda4c657f961fe4089224babd9966412952e81
vteze <v.cruz001@edu.pucrs.br>
2022-11-01 17:11:15 -0300

    removing unnecessary imports

--- Tarefa2Emprestimos/src/test/java/com/bcopstein/Emprestimos/EmprestimoTest.java
+++ Tarefa2Emprestimos/src/test/java/com/bcopstein/Emprestimos/EmprestimoTest.java
@@ -1,23 +1,13 @@
 package com.bcopstein.Emprestimos;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import com.bcopstein.Emprestimos.Emprestimo.Builder;
 
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
